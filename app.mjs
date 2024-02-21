30058f22af75f7a400c9430cfcd51e637574b828
Zach Meyers <zach@zachmeyers.com>
2024-02-21 12:22:38 -0600

    initial commit

---
+++ app.mjs
@@ -0,0 +1,88 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dbService from './services/db.mjs';
+import { auth } from 'express-oauth2-jwt-bearer';
+
+const jwtCheck = auth({
+  audience: 'http://localhost:3000',
+  issuerBaseURL: 'https://hockey-warehouse.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
+// app.use(jwtCheck);
+
+// GET all players
+app.get('/players', (req, res) => {
+  dbService.getAll('players').then(players => {
+    res.json(players);
+  });
+});
+
+// GET player by _id
+app.get('/players/:id', (req, res) => {
+  dbService.getOne('players', req.params.id).then(player => {
+    if (!player) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+    res.json(player);
+  });
+});
+
+// POST player
+app.post('/players', (req, res) => {
+  dbService.addOne('players', req.body).then(player => {
+    res.send(player.insertedId);
+  });
+});
+
+// POST list of players
+app.post('/players', (req, res) => {
+  dbService.addMany('players', req.body).then(result => {
+    res.send(result);
+  });
+});
+
+// DELETE player
+app.delete('/players/:id', (req, res) => {
+  dbService.delete
+});
+
+// GET all divisions
+app.get('/divisions', (req, res) => {
+  dbService.getAll('divisions').then(result => {
+    res.send(result);
+  });
+});
+
+// GET division by _id
+app.get('/divisions/:id', (req, res) => {
+  dbService.getOne('divisions', req.params.id).then(division => {
+    if (!division) {
+      return res.status(404).json({ error: 'Division not found' });
+    }
+    res.json(player);
+  });
+});
+
+// POST division
+app.post('/divisions', (req, res) => {
+  dbService.addOne('divisions', req.body).then(player => {
+    res.send(player.insertedId);
+  });
+});
+
+// POST list of divisions
+app.post('/divisions', (req, res) => {
+  dbService.addMany('divisions', req.body).then(result => {
+    res.send(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ services/db.mjs
@@ -0,0 +1,107 @@
+import { MongoClient } from "mongodb";
+import * as mongo from "mongodb";
+const uri = "mongodb+srv://web:Password1@devcluster-1.oya5nr1.mongodb.net/";
+const dbName = "hockey";
+
+export async function getAll(collectionName) {
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Access the database and collection
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    // Fetch all players
+    const players = await collection.find({}).toArray();
+
+    return players;
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+}
+
+export async function getOne(collectionName, id) {
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Access the database and collection
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    // Fetch all players
+    const player = await collection.findOne({
+      _id: new mongo.ObjectId(id),
+    });
+
+    return player;
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+}
+
+export async function addMany(collectionName, items) {
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Access the database and collection
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    await collection.insertMany(items);
+    return `${items.length} ${collectionName} added`;
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+}
+
+export async function addOne(collectionName, item) {
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Access the database and collection
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    const result = await collection.insertOne(item);
+    return result;
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+}
+
+export async function deleteOne(collectionName, id) {
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Access the database and collection
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    const result = await collection.deleteOne({_id: id});
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+
+    return result;
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+}
